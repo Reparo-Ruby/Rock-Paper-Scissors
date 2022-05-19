68f4b32a2a3160320d37f40e0995fc9eb30829a0
Sinesethu <stukani021@students.wethinkcode.co.za>
2022-05-19 15:54:50 +0200

    fjhtyyfuj

--- RPS.py
+++ RPS.py
@@ -1,4 +1,5 @@
 import random
+
 def game_welcome():
     print("""Welcome to Rock, Paper, Scissors.
 In this game you have three round to try and win agains your opponet whom is the computer.
@@ -6,84 +7,137 @@ In cases of a tie a round is not lost and neither of you get a point.
                             LET THE GAMES BEGIN
     """)
 
-def who_wins(com,you):
-    print(f"""GAME RESULTS
-{com}:{you}""")
+
+def player_guess():
+    userhand = input("Pick your fighter, [R]ock, [P]aper, [S]cissor.\n")
+    return userhand
+
+
+def check_guess(user_guess):
     
-    if com > you:
+    if user_guess == "R":
+        user_guess = "Rock"
+        return user_guess
+
+    elif user_guess == "P":
+        user_guess = "Paper"
+        return user_guess
+
+    elif user_guess == "S":
+        user_guess = "Scissor"
+        return user_guess
+
+    else:
+        invalid_input()
+        return False
+
+
+def evaluate_rock(computer,rounds,userhand2):
+
+    if userhand2 == "Rock":
+        rounds -=1
+        if computer == "Paper":
+            print("Paper beats Rock!\n")
+            return True
+        elif computer == "Scissor":
+            print("Rock beats Scissor!\n")
+            return False
+        elif computer == "Rock":
+            rounds += 1
+            print("Its A Tie!\n")
+    return rounds   
+
+
+def evaluate_paper(computer,rounds,userhand2):
+
+    if userhand2 == "Paper":
+        rounds -=1
+        if computer == "Rock":
+            print("Paper beats Rock!\n")
+            return True
+        elif computer == "Scissor":
+            print("Scissor beats Paper!\n")
+            return False
+        elif computer == "Paper":
+            rounds += 1
+            print("Its A Tie!\n")
+    return rounds        
+            
+
+def evaluate_scissor(computer,rounds,userhand2):
+
+    if userhand2 == "Scissor":
+        rounds -=1
+        if computer == "Paper":
+            print("Scissor beats Paper!\n")
+            return True
+        elif computer == "Rock":
+            print("Rock beats Scissor!\n")
+            return False
+        elif computer == "Scissor":
+            rounds += 1
+            print("Its A Tie!\n")
+    return rounds 
+
+
+def who_wins(computer,player):
+
+    print(f"""GAME RESULTS
+{computer}:{player}""")
+    
+    if computer > player:
         print("Your Opponent Won!")
-    elif com < you:
+    elif computer < player:
         print("You Win!!")
 
- 
 
 def invalid_input():
+
     input = """******************************************************
 Game only understands [R]ock, [P]aper,[S]cissors.
 ******************************************************"""
+    print(input)
+    return 
     
-    return input
 
+def evaluate_responses(computer_pick,computer_wins,user_wins):
 
-def evaluate_responses(computer_pick,c_wins,u_win):
     rounds = 3
 
     while rounds != 0:
         computer = random.choice(computer_pick)
-        userhand = input("Pick your fighter, [R]ock, [P]aper, [S]cissors.\n")
-        hand = userhand.upper()
+        userhand2 = check_guess(player_guess())
 
-        if hand == "R":
-            rounds -=1
-            if computer == "Paper":
-                print("Paper beats Rock!\n")
-                c_wins += 1
-            elif computer == "Scissors":
-                print("Rock beats Scissors!\n")
-                u_win += 1
-            elif computer == "Rock":
-                rounds += 1
-                print("Its A Tie!\n")
-            continue
+        if userhand2 == False:
+            break
 
-        elif hand == "P":
-            rounds -=1
-            if computer == "Rock":
-                print("Paper beats Rock!\n")
-                u_win += 1
-            elif computer == "Scissors":
-                print("Scissors beats Paper!\n")
-                c_wins += 1
-            elif computer == "Paper":
-                rounds += 1
-                print("Its A Tie!\n")
-            continue
-
-        elif hand == "S":
-            rounds -=1
-            if computer == "Paper":
-                print("Scissors beats Paper!\n")
-                u_win += 1
-            elif computer == "Rock":
-                print("Rock beats Scissors!\n")
-                c_wins += 1
-            elif computer == "Scissors":
-                rounds += 1
-                print("Its A Tie!\n")
+        if evaluate_rock(computer,rounds,userhand2):
+            if True:
+                user_wins +=1
             continue
         
+        elif evaluate_paper(computer,rounds,userhand2):
+            if True:
+                user_wins +=1
+            continue
+
+        elif evaluate_scissor(computer,rounds,userhand2):
+            if True:
+                user_wins +=1
+            continue
+    
         else:
-            response = invalid_input()
-            print(response)
+            computer_wins += 1
 
-    return c_wins,u_win
+        return computer_wins,user_wins    
         
-
+          
 def run_game():
-    hands = ["Rock","Paper","Scissors"]
+    hands = ["Rock","Paper","Scissor"]
     game_welcome()
     com,you = evaluate_responses(hands,0,0)
     who_wins(com,you)
+
 
 if __name__ == "__main__":
     run_game()
---
+++ RPS_test.py
@@ -0,0 +1,5 @@
+import unittest
+import RPS # Import the file that you want to test
+from unittest.mock import patch
+from io import StringIO
+import sys
